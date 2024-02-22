dfe303e5592d47e49cd796c727a9a78764b73586
Dragonfly-Maeda <jokarkuii@gmail.com>
2024-02-23 05:25:50 +0900

    初期化と毎フレームの処理を区別化

--- sample.js
+++ sample.js
@@ -1,36 +1,59 @@
-// phina.js をグローバル領域に展開
+// phina.jsの初期化
 phina.globalize();
 
-// MainScene クラスを定義
+
 phina.define('MainScene', {
-  superClass: 'DisplayScene',
-  init: function() {
-    this.superInit();
-    // 背景色を指定
-    this.backgroundColor = '#222';
+    superClass: 'DisplayScene',
 
-    // 星を表示
-    for (let i=0; i<32; ++i) {
-      let star = StarShape().addChildTo(this);
-      star.x = Math.randint(0, this.width);
-      star.y = Math.randint(0, this.height);
-      star.rotation = Math.randint(0, 360);
+    // 初期化
+    init: function () {
+        this.superInit();
 
-      star.stroke = 'transparent';
+        var self = this;
 
-      star.update = function() {
-        this.rotation+=4;
-      };
-    }
-  },
+        // ブロックを作成
+        this.block = RectangleShape({
+            width: 100,
+            height: 100,
+            fill: 'blue',
+        }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
+
+        // ブロックが動いているかどうかのフラグ
+        this.isMoving = false;
+
+        // タッチイベントの設定
+        this.block.setInteractive(true);
+        this.block.on('pointstart', function () {
+            // ブロックがタッチされたら動かす
+            if (!self.isMoving) {
+                self.isMoving = true;
+            }
+        });
+    },
+
+    // 毎フレームごとの更新
+    update: function (app) {
+        // ブロックを動かす
+        if (this.isMoving) {
+            this.block.x += 2; // 移動速度を設定
+
+            // 画面外に出たら停止する
+            if (this.block.right > this.gridX.width) {
+                this.isMoving = false;
+            }
+        }
+    },
 });
 
-// メイン処理
 phina.main(function() {
-  // アプリケーション生成
-  var app = GameApp({
-    startLabel: 'main', // メインシーンから開始する
+    // アプリケーションを生成
+  
+    var app = GameApp({
+      startLabel: 'main', // MainScene から開始
+    });
+  
+    //app.enableStats();
+  
+    // 実行
+    app.run();
   });
-  // アプリケーション実行
-  app.run();
-});
--- test.js
+++ test.js
@@ -3,7 +3,6 @@ phina.globalize();
 /*
  * メインシーン
  */
-var label1_move = false;
 var unicodeUnescape = function(str) {
 	var result = '', strs = str.match(/\\u.{4}/ig);
 
@@ -24,69 +23,67 @@ phina.define("MainScene", {
   init: function() {
     // super init
     this.superInit();
+    var self = this;
 
     // 背景色
     this.backgroundColor = '#e0ffce';
     let url = new URL(window.location.href);
     let params = url.searchParams;
-    // ラベルを生成
-    var shape = Shape().addChildTo(this);
-    shape.setPosition(200, 300);
-    shape.setSize(180, 60);
-    shape.backgroundColor = '#999';
-    var label1 = Label(unicodeUnescape(params.get('name'))).addChildTo(this);
-    label1.setPosition(200, 300);
-    label1.fontSize = 48;
+
+    //名前背景の四角
+    this.shape = Shape().addChildTo(this);
+    this.shape.setPosition(200, 300);
+    this.shape.setSize(180, 60);
+    this.shape.backgroundColor = '#999';
+
+    //フラグ初期化
+    this.label1_move = false;
+    //ブロックがタッチできるように
+    this.shape.setInteractive(true);
+    //ブロックがタッチされたら動かす
+    this.shape.onpointstart = function() {
+      self.label1_move = true;
+      alert("タッチ");
+    };
+
+    //名前
+    this.label1 = Label(unicodeUnescape(params.get('name'))).addChildTo(this);
+    this.label1.setPosition(200, 300);
+    this.label1.fontSize = 48;
     // label1.backgroundColor = '#999999'
-    label1.strokeWidth = 2;
-    label1.stroke = '#000';
-    label1.fill = '#0f0';  // 塗りつぶし色
-    shape.setInteractive(true);
-    shape.onpointstart = function() {
-      label1_move = true;
-    };
-    
-    var label2 = Label(unicodeUnescape(params.get('txt'))).addChildTo(this);
-    label2.x = this.gridX.center(); // x 軸
-    label2.y = this.gridY.center(); // y 軸
-    label2.fontSize = 34;
-    label2.fill = '#000';  // 塗りつぶし色
-    label2.update = function() {
-      if(label2.y < 100){
-        label2.remove();
-      }
-      if(label1_move){
-        label2.y -= 5;
-      }
-    };
+    this.label1.strokeWidth = 2;
+    this.label1.stroke = '#000';
+    this.label1.fill = '#0f0';  // 塗りつぶし色
 
-    var label3 = Label('前田悠翔').addChildTo(this);
-    label3.setPosition(500, 700);
-    label3.fontSize = 40;
-    label3.fill = '#000';  // 塗りつぶし色
-    label3.update = function() {
-      if(label3.y < 100){
-        label3.remove();
-      }
-      if(label1_move){
-        label3.y -= 5;
-      }
-    };
+    //本文
+    this.label2 = Label(unicodeUnescape(params.get('txt'))).addChildTo(this);
+    this.label2.x = this.gridX.center(); // x 軸
+    this.label2.y = this.gridY.center(); // y 軸
+    this.label2.fontSize = 34;
+    this.label2.fill = '#000';  // 塗りつぶし色
+
+    //自分の名前
+    this.label3 = Label('前田悠翔').addChildTo(this);
+    this.label3.setPosition(500, 700);
+    this.label3.fontSize = 40;
+    this.label3.fill = '#000';  // 塗りつぶし色
+  
   },
-
-  // update: function() {
-  //   // if(label1_move == true){
-  //     // 下に移動
-  //     label1.vy += 0.98;
-  //     label1.y += label1.vy;
-
-  //     // // 地面に着いたら反発する
-  //     // if (label1.bottom > FLOOR_HEIGHT) {
-  //     //   label1.bottom = FLOOR_HEIGHT;
-  //     //   label1.vy *= -1;
-  //     // }
-  //   // }
-  // },
+  
+  //フレーム更新
+  update: function(app) {
+    var p = app.pointer;
+    
+    if(this.label1_move){
+      this.label2.y -= 5;
+      this.label3.y -= 5;
+      //alert('1');
+    }
+    if(this.label3.y < 100){
+      this.label3.remove();
+      this.label2.remove();
+    }
+  },
 });
 
 /*
